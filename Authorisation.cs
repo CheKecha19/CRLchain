6aac43052574be77a0df47ae4d38a29497c9d880
Никита Назаров <chechecherokee@gmail.com>
2021-02-04 21:46:37 +0300

    C#  Добавлены окно авторизации, ошибки, главное окноДобавьте файлы проекта.

---
+++ Authorisation.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRLchain
+{
+    public partial class Authorisation : Form
+    {
+        public Authorisation()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //проверка на заполненность
+            if (textBox1.Text == null || maskedTextBox1.Text == null)
+            {
+                SendError();
+            }
+            else
+            {
+                try
+                {
+
+                }
+                catch
+                {
+                    SendError();
+                }
+            }
+            //проверка на правильность
+
+
+        }
+
+        private void SendError()
+        {
+            ErrorMessage error = new ErrorMessage();
+            Show(error);
+        }
+    }
+}
